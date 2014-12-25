b756f267f2c2335077d301ad4662a3cb55e6acfd
Jonathan Primeau <jonathan.primeau@gmail.com>
2014-12-25 00:57:05 -0500

    WIP

--- tools/assembler.py
+++ tools/assembler.py
@@ -7,55 +7,81 @@ instructions = grammar.keys()
 
 num = 1
 address = 0
-label_dict = {}
+labels = {}
+dlines = []
 
 f = open('test.asm', 'r')
+
 for line in f:
-    opcode = ''
+    # Find and extract label
+    r = re.split(':?\s', line)
+    if r and r[0] != '' and r[0][0] != ';':
+        label = r[0]
+        labels[label] = ''
+
+f.seek(0)
+
+for line in f:
     label = ''
     mnemonic = ''
-    destination = ''
-    source = ''
+    dst_src = ''
     comment = ''
+    opcode = ''
+    byte_lo = ''
+    byte_hi = ''
     dline = {}
 
-    # Check and extract a label
+    # Find and extract label
     r = re.split(':?\s', line)
     if r and r[0] != '' and r[0][0] != ';':
         label = r[0]
     dline['label'] = label
+    # Remove label
+    line = re.sub('^.*:', '', line)
+    # Remove leading spaces
+    line = re.sub('^\s*', '', line)
 
-    # Check and extract a comment
+    # Find and extract comment
     r = re.search(';.*$', line)
     if r:
         comment = r.group(0)
     dline['comment'] = comment
+    # Remove comment and spaces
+    line = re.sub('\s*;.*$', '', line)
 
-    # Remove comments
-    cline = re.sub(';.*$', '\r\n', line)
-    # Remove trailing spaces
-    cline = re.sub('\s*$', '\r\n', cline)
     for inst in instructions:
-        match = re.search('\s'+inst+'\s', cline)
+        match = re.match(inst+'(\s|$)', line)
         if match:
+            # Remove mnemonic and spaces
+            line = re.sub(match.group(0), '', line)
+            # Remove spaces
+            line = re.sub('\s*', '', line)
             op = grammar[inst]
             optype = op[0]
             opsize = int(op[1])
             tmp_opcode = op[2]
+            t_dst_src = line
             if optype == "R,D":
                 regs = tmp_opcode.keys()
                 for reg in regs:
-                    match = re.search('\s*'+reg+'\s*,', cline)
-                    if match:
-                        mnemonic = inst
-                        opcode = tmp_opcode[reg]
-                        address += opsize
+                    m_dst = re.match(reg+',', line)
+                    if m_dst:
+                        # Remove destination
+                        line = re.sub(m_dst.group(0), '', line)
+                        m_src = re.search('[0-9A-Fa-f]+(h|H)', line)
+                        if m_src:
+                            dst_src = t_dst_src
+                            byte_lo = "%02X" % int(m_src.group(0)[:-1], 16)
+                            mnemonic = inst
+                            opcode = tmp_opcode[reg]
+                            address += opsize
                         break
             elif optype == 'R,R':
                 regs = tmp_opcode.keys()
                 for reg in regs:
-                    match = re.search('\s*'+reg+'\s*', cline)
+                    match = re.search(reg, line)
                     if match:
+                        dst_src = t_dst_src
                         mnemonic = inst
                         opcode = tmp_opcode[reg]
                         address += opsize
@@ -63,8 +89,9 @@ for line in f:
             elif optype == 'Rp':
                 regs = tmp_opcode.keys()
                 for reg in regs:
-                    match = re.search('\s*'+reg+'\s*', cline)
+                    match = re.search(reg, line)
                     if match:
+                        dst_src = t_dst_src
                         mnemonic = inst
                         opcode = tmp_opcode[reg]
                         address += opsize
@@ -72,35 +99,78 @@ for line in f:
             elif optype == 'R':
                 regs = tmp_opcode.keys()
                 for reg in regs:
-                    match = re.search('\s*'+reg+'\s*', cline)
+                    match = re.search(reg, line)
                     if match:
+                        dst_src = t_dst_src
                         mnemonic = inst
                         opcode = tmp_opcode[reg]
                         address += opsize
                         break
             elif optype == 'P':
-                match = re.search('\s+([0-9A-Fa-f]+)', cline)
+                match = re.search('[0-9A-Fa-f]+(h|H)?', line)
                 if match:
+                    dst_src = t_dst_src
+                    byte_lo = "%02X" % int(match.group(0), 16)
                     mnemonic = inst
                     opcode = tmp_opcode
                     address += opsize
                     break
             elif optype == 'A':
-                match = re.search('\s+([0-9A-Fa-f]{1,2})', cline)
-                if match:
-                    mnemonic = inst
-                    opcode = tmp_opcode
-                    address += opsize
-                    break
-                match = re.search('\s+([0-9A-Fa-f]*)', cline)
-                if match:
-                    mnemonic = inst
-                    opcode = tmp_opcode
-                    address += opsize
-                    break
+                lbl_found = False
+                for lbl in labels:
+                    m_lbl = re.search(lbl, line)
+                    if m_lbl:
+                        dst_src = t_dst_src
+                        lbl_found = True
+                        mnemonic = inst
+                        opcode = tmp_opcode
+                        address += opsize
+                        break
+                if not lbl_found:
+                    match = re.search('[0-9A-Fa-f]{1,2}(h|H)?', line)
+                    if match:
+                        dst_src = t_dst_src
+                        tmp = "%04X" % int(match.group(0), 16)
+                        byte_lo = tmp[2:4]
+                        byte_hi = tmp[0:2]
+                        mnemonic = inst
+                        opcode = tmp_opcode
+                        address += opsize
+                        break
+            elif optype == 'Rp,DD':
+                regs = tmp_opcode.keys()
+                for reg in regs:
+                    m_dst = re.match(reg+',', line)
+                    if m_dst:
+                        # Remove destination
+                        line = re.sub(m_dst.group(0), '', line)
+                        lbl_found = False
+                        for lbl in labels:
+                            m_lbl = re.search(lbl, line)
+                            if m_lbl:
+                                lbl_found = True
+                                dst_src = t_dst_src                                
+                                mnemonic = inst
+                                opcode = tmp_opcode[reg]
+                                address += opsize
+                                break
+                        if not lbl_found:
+                            match = re.search('[0-9A-Fa-f]{1,2}(h|H)?', line)
+                            if match:
+                                dst_src = t_dst_src
+                                tmp = "%04X" % int(match.group(0), 16)
+                                byte_lo = tmp[2:4]
+                                byte_hi = tmp[0:2]
+                                mnemonic = inst
+                                opcode = tmp_opcode[reg]
+                                address += opsize
+                                break
+                        break
             elif optype == 'D':
-                match = re.search('\s+([0-9A-Fa-f]+)', cline)
+                match = re.search('[0-9A-Fa-f]+(h|H)?', line)
                 if match:
+                    dst_src = t_dst_src
+                    byte_lo = "%02X" % int(match.group(0)[:-1], 16)
                     mnemonic = inst
                     opcode = tmp_opcode
                     address += opsize
@@ -112,18 +182,52 @@ for line in f:
             break
     
     dline['mnemonic'] = mnemonic
-    dline['destination'] = destination
-    dline['source'] = source
+    dline['dst_src'] = dst_src
     dline['opcode'] = opcode
+    dline['byte_lo'] = byte_lo
+    dline['byte_hi'] = byte_hi
     dline['address'] = "%04X"%address
-
     dline['line'] = "%05d"%num
     num += 1
 
     if label != '':
-        label_dict[label] = "%04X"%address
+        labels[label] = "%04X"%address
+    dlines.append(dline)
 
-    #print dline
-    print dline['line'],dline['address'],dline['opcode']
-print label_dict
+byte_array = ['FF']*256
+idx = 0
+for dline in dlines:
+    # Replace label in operand by value
+    label = re.sub('.*,\s*', '', dline['dst_src'])
+    if label in labels:
+        dline['byte_lo'] = labels[label][2:4]
+        dline['byte_hi'] = labels[label][0:2]
+    if dline['opcode']:
+        byte_array[idx] = dline['opcode']
+        idx += 1
+    if dline['byte_lo']:
+        byte_array[idx] = dline['byte_lo']
+        idx += 1
+    if dline['byte_hi']:
+        byte_array[idx] = dline['byte_hi']
+        idx += 1
+    print "%5s"%dline['line'],
+    print "%4s"%dline['address'],
+    print "%2s"%dline['opcode'],
+    print "%2s"%dline['byte_lo'],
+    print "%2s"%dline['byte_hi'],
+    print "%20s"%dline['label'],
+    print "%-4s"%dline['mnemonic'],
+    print "%-24s"%dline['dst_src'],
+    print dline['comment']
+print labels
+# x"C3",x"18",x"00",x"FF",x"FF",x"FF",x"FF",x"FF", -- 00H
+#num = 0
+#for byte in byte_array:
+#    if num == 7:
+#        print 'x"'+byte+'",'
+#        num = 0
+#    else:
+#        print 'x"'+byte+'",',
+#        num += 1
 
--- tools/test.asm
+++ tools/test.asm
@@ -1,7 +1,7 @@
-;			.org	00000h
-;Start_of_RAM:		.eq	0x0800
+			.org	00000h
+Start_of_RAM:		.eq	0x0800
 			JMP 	Get_address		;Skip over message
-;			defm	"JP-80 ROM v.1",0
+			defm	"JP-80 ROM v1.0",0
 Get_address:		IN	0			;Get address from input ports
 			MOV	L,A
 			IN 	1
@@ -12,7 +12,7 @@ Port_Reflector:		IN 	0			;Simple program to test ports
 			IN 	1
 			OUT 	1
 			JMP	Port_Reflector
-Simple_Counter:		LDA 	000h			;One-byte counter for slow clock
+Simple_Counter:		MVI 	A,000h			;One-byte counter for slow clock
 Loop_1:			OUT 	0
 			INR 	A
 			JMP 	Loop_1
@@ -21,9 +21,45 @@ Count_to_a_million:	MVI 	L,000h			;Two-byte (16-bit) counter
 Loop_2:			MVI 	A,010h			;Count 16 times, then
 Loop_3:			DCR 	A
 			JNZ 	Loop_3
-			INR 	H			;increment the 16-bit number
+			INX 	H			;increment the 16-bit number
 			MOV 	A,L
 			OUT 	0			;Output the 16-bit number
 			MOV 	A,H
 			OUT 	1
 			JMP 	Loop_2			;Do it again
+Program_loader:		LXI 	H,Start_of_RAM		;Load a program in RAM
+Loop_4:			IN 	1
+			ANI 	081h			;Check input port 1
+			JZ 	Loop_4			;If switches 0 and 7 open, loop
+			CALL 	debounce
+			IN 	1			;Get input port byte again
+			ANI 	080h			;Is the left switch (bit 7) closed?
+			JNZ 	Start_of_RAM		;Yes, run loaded program
+			IN 	0			;No, then right switch (bit 0) closed.
+			OUT 	0			;Get byte from port 0, display on output
+			MOV 	M,A			;Store it in RAM
+			MVI 	A,0ffh			;Turn port 1 lights on (signal that
+			OUT 	1			;a byte was stored)
+Loop_6:			IN 	1			;Wait for switch to open
+			ANI 	001h
+			JNZ 	Loop_6
+			CALL 	debounce
+			MOV 	A,L			;Put low byte of address on port 1
+			OUT 	1
+			INX 	H			;Point to next location in RAM
+			JMP 	Loop_4			;Do it again
+Memory_test:		LXI 	H,Start_of_RAM		;check RAM by writing and reading each location
+Loop_8:			IN 	1			;read port 1 to get a bit pattern
+			MOV 	B,A			;copy it to register b
+			MOV 	M,A			;store it in memory
+			MOV 	A,M			;read back the same location
+			CMP 	B			;same as reg b?
+			JNZ 	Exit_1			;no, test failed, exit
+			INX 	H			;yes, RAM location OK
+			JMP 	Loop_8			;keep going
+Exit_1:			MOV 	A,H			;display the address
+			OUT 	1			;where the test failed
+			MOV 	A,L			;should be 4K (cycled around to ROM)
+			OUT 	0			;any other value means bad RAM
+			JMP 	Memory_test		;do it again (use a different bit pattern)
+
